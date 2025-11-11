4842c9dc2dff519de95a673ca403fdf95c6311ca
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-11-11 19:53:07 +0530

    Update generateToken.js

--- jwt/generateToken.js
+++ jwt/generateToken.js
@@ -7,10 +7,11 @@ import jwt from 'jsonwebtoken';
 //  console.log(token ,"token generatre")
  res.cookie("jwt",token,{
     httpOnly:true,
-    secure:false,
+    secure:process.env.NODE_ENV === "production",
     sameSite:'lax',
     maxAge:180*24*60*60*1000 //180 days
  })
 }
+
 
 export default createTokenAndSaveCookie;
