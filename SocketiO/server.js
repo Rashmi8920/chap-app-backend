0038e6edc3c575c694135ce999f0bb26ab0d9ddf
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-11-12 20:13:30 +0530

    Update server.js

--- SocketiO/server.js
+++ SocketiO/server.js
@@ -8,6 +8,7 @@ const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
         origin:["http://localhost:5173",
+        "http://localhost:5174",
         "https://chat-application-52gd.onrender.com"],
         methods:["GET","POST"],
           credentials: true, 
@@ -41,5 +42,6 @@ io.emit("getOnlineUsers", Object.keys(users));
     })
 })  
 export {app,io,server}
+
 
 
